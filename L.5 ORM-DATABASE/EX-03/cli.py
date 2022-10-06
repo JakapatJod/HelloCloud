7df66d8750be1514354ffe9778a68116fbb9926f
JakapatJod <jakapat2332@gmail.com>
2022-10-06 15:38:24 +0700

    SS

--- L.5 ORM-DATABASE/EX-03/cli.py
+++ L.5 ORM-DATABASE/EX-03/cli.py
@@ -2,15 +2,15 @@ from crud import Session
 from models import Book
 
 s = Session()
-ิbooks = s.query(Book).all()
+books = s.query(Book).all()
 
 for book in books:
     if book.price == None:
         price = input(
-            f"Price for '{book.title}': Old Price:'{book.price}' New Price : $"
+            f"Price for '{book.title}': Old Price:'{book.price}' New Price : $")
         book.price = price
         s.add(book)
-        )
+        
 
 s.commit()
 s.close()
